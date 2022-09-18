28fb7280baa45e027800892a9e51d51ffccb31cf
theikchan <theikchan.tc@gmail.com>
2022-09-18 12:37:18 +0630

    test html text

--- src/Main.elm
+++ src/Main.elm
@@ -1,5 +1,47 @@
 module Main exposing (..)
-import Html exposing (..)
 
+import Html exposing (Html, div, p, text)
+
+
+
+-- type alias Model =
+--     { message : String }
+-- type Msg =
+--     Message String
+-- model : Model
+-- model =
+--     Model ""
+-- update : Msg -> Model -> Model
+-- update msg model =
+--     case msg of
+--         Message newMessage ->
+--             { model | message = newMessage }
+-- view : Model -> Html
+-- view model =
+--     div [] []
+
+
+showUserName : String -> Html msg
+showUserName name =
+    div []
+        [ text "Name: "
+        , text name
+        ]
+
+
+showUserEmail : String -> Html msg
+showUserEmail email =
+    div []
+        [ text "Email Address: "
+        , text email
+        ]
+
+
+main : Html msg
 main =
-    text "Hello World!"
+    div []
+        [ text "Hello World!"
+        , p [] [ text "Hello Elm" ]
+        , showUserName "T C"
+        , showUserEmail "tc@gmail.com"
+        ]
